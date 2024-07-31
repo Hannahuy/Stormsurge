9e047b0b2cacd299461131e473bc9297a23539ad
Hannahuy <hanyu20011010@icloud.com>
2024-07-31 15:02:41 +0800

    分辨率

--- src/components/topButton/background.vue
+++ src/components/topButton/background.vue
@@ -3,10 +3,28 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, onBeforeUnmount, ref } from 'vue';
 import { app_load, callUIInteraction } from '../../module/webrtcVideo/webrtcVideo.js';
 import '../../module/webrtcVideo/player.css';
+
+const width = ref()
+const height = ref()
+
+const logScreenSize = () => {
+    width.value = window.innerWidth;
+    height.value = window.innerHeight;
+    callUIInteraction({
+        ModuleName: '其他',
+        FunctionName: "分辨率",
+        Width:width.value,
+        Height:height.value
+    });
+};
+
 onMounted(() => {
+    logScreenSize(); // 初始打印屏幕大小
+    window.addEventListener('resize', logScreenSize); // 监听屏幕大小变化
+
     app_load(
         {
             serverUrl: 'http://192.168.0.124:89/', // 像素流链接
@@ -22,7 +40,7 @@ onMounted(() => {
         () => {
             console.log("成功");
             callUIInteraction({
-                ModuleName:'其他',
+                ModuleName: '其他',
                 FunctionName: "刷新",
             });
         },
@@ -30,6 +48,10 @@ onMounted(() => {
             console.log("失败");
         }
     );
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', logScreenSize); // 清理事件监听器
 });
 </script>
 
