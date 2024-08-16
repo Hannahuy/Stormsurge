446a3c0e524f75dddaf468e87dfff723060d960e
Hannahuy <hanyu20011010@icloud.com>
2024-08-16 16:02:01 +0800

    假设分析更改

--- src/components/topButton/Whatifanalysis.vue
+++ src/components/topButton/Whatifanalysis.vue
@@ -6,7 +6,7 @@
             </div>
             <div class="oneBox">
                 <el-checkbox-group v-model="checkListone" class="checkboxone"
-                    @change="handleCheckChange('checkListone')">
+                    @change="handleCheckChange('checkListone')" :disabled="isCheckListmeasure">
                     <el-checkbox label="无减灾设施" value="无减灾设施" />
                     <el-checkbox label="加防潮堤" value="加防潮堤" />
                     <el-checkbox label="加防汛沙袋" value="加防汛沙袋" />
@@ -28,19 +28,19 @@
             <div class="leftbox-top-title">
                 <span>重现期情景库</span>
             </div>
-            <div class="radiobox">
-                <el-radio-group v-model="radio1" @change="getradio">
-                    <el-radio value="1" size="large">海浪情景</el-radio>
-                    <el-radio value="2" size="large">淹没情景</el-radio>
-                </el-radio-group>
+            <div class="checkbox">
+                <el-checkbox-group v-model="checkList" @change="getcheck" :disabled="isCheckListscene">
+                    <el-checkbox label="海浪情景" value="海浪情景" />
+                    <el-checkbox label="淹没情景" value="淹没情景" />
+                </el-checkbox-group>
                 <div class="switchbox" v-if="showdimension">
-                    <el-switch v-model="dimensionvalue" class="ml-2" inline-prompt active-text="二维" inactive-text="三维"
-                        @change="getdimension" />
+                    <el-switch v-model="dimensionvalue" class="ml-2" inline-prompt active-text="二维"
+                        inactive-text="三维" />
                 </div>
             </div>
             <div class="twoBox">
                 <el-checkbox-group v-model="checkListtwo" class="checkboxtwo"
-                    @change="handleCheckChange('checkListtwo')">
+                    @change="handleCheckChange('checkListtwo')" :disabled="isCheckListEmpty">
                     <div class="checkbox-column">
                         <el-checkbox label="10年一遇" value='10' />
                         <el-checkbox label="20年一遇" value='20' />
@@ -58,48 +58,66 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch, onMounted, computed } from 'vue';
 import { callUIInteraction } from "../../module/webrtcVideo/webrtcVideo.js";
 import { ElMessage } from 'element-plus';
 
-
 const checkListone = ref([]);
-const checkListtwo = ref(['10']);
+const checkListtwo = ref([]);
 const inputvalue = ref(0.0);
-const radio1 = ref('1')
-const dimensionvalue = ref(true)
-const showdimension = ref(true)
-watch(radio1, (newValue) => {
-    if (newValue === '2') {
-        showdimension.value = false;
+const checkList = ref([]);
+const dimensionvalue = ref(true);
+const showdimension = ref(false);
+const isCheckListEmpty = computed(() => checkList.value.length === 0);
+const isCheckListscene = computed(() => checkListone.value.length != 0);
+const isCheckListmeasure = computed(() => checkList.value.length != 0);
+watch(checkList, (newValue) => {
+    if (newValue.length === 0) {
+        checkListtwo.value = [];
     } else {
+        checkListtwo.value = ['10'];
+    }
+
+    if (newValue[0] === '海浪情景') {
         showdimension.value = true;
+    } else {
+        showdimension.value = false;
     }
 });
-let lastSelected = ref('0m'); // 用于跟踪上一个选中的选项
+
+let lastSelected = ref('0m');
 
 const descriptions = {
     checkListone: '减灾措施情景库',
     checkListtwo: '重现期情景库',
 };
-const getradio = (e) => {
-    checkListtwo.value = [10]
-    if (e === '1') {
-        callUIInteraction({
-            ModuleName: '假设分析',
-            FunctionName: `${descriptions['checkListtwo']}`,
-            ChildrenModule: scene,
-        });
-        console.log('假设分析', `${descriptions['checkListtwo']}`, scene);
+
+const getcheck = (checked) => {
+    if (checked.length > 1) {
+        checkList.value = [checked[checked.length - 1]];
     } else {
+        checkList.value = checked;
+    }
+    if (checkList.value[0] === '海浪情景') {
         callUIInteraction({
             ModuleName: '假设分析',
             FunctionName: `${descriptions['checkListtwo']}`,
-            ChildrenModule: scene,
+            ChildrenModule: checkList.value[0],
+            State: checkListtwo.value[0],
+            Dimension: isdimension
         });
-        console.log('假设分析', `${descriptions['checkListtwo']}`, scene);
+        console.log('假设分析', `${descriptions['checkListtwo']}`, checkList.value[0], checkListtwo.value[0], isdimension);
+    } else if (checkList.value[0] === '淹没情景') {
+        callUIInteraction({
+            ModuleName: '假设分析',
+            FunctionName: `${descriptions['checkListtwo']}`,
+            ChildrenModule: checkList.value[0],
+            State: checkListtwo.value[0]
+        });
+        console.log('假设分析', `${descriptions['checkListtwo']}`, checkList.value[0], checkListtwo.value[0]);
     }
-}
+};
+
 const getinput = () => {
     if (inputvalue.value == null) {
         ElMessage({
@@ -114,34 +132,10 @@ const getinput = () => {
         State: inputvalue.value
     });
     console.log('假设分析', `水位情景库`, inputvalue.value);
-}
-const getdimension = () => {
-    if (dimensionvalue.value) {
-        callUIInteraction({
-            ModuleName: '假设分析',
-            FunctionName: `${descriptions['checkListtwo']}`,
-            ChildrenModule: scene,
-            Dimension: `二维`,
-            State: true
-        });
-        console.log('假设分析', `${descriptions['checkListtwo']}`, scene, `二维`, true);
-    } else {
-        callUIInteraction({
-            ModuleName: '假设分析',
-            FunctionName: `${descriptions['checkListtwo']}`,
-            ChildrenModule: scene,
-            Dimension: `三维`,
-            State: true
-        });
-        console.log(`${descriptions['checkListtwo']}`, scene, `三维`, true);
-    }
-}
-let scene = '';
-if (radio1.value === '1') {
-    scene = '海浪情景';
-} else if (radio1.value === '2') {
-    scene = '淹没情景';
-}
+};
+
+
+
 onMounted(() => {
     callUIInteraction({
         ModuleName: '假设分析',
@@ -150,26 +144,11 @@ onMounted(() => {
     });
     callUIInteraction({
         ModuleName: '假设分析',
-        FunctionName: `${descriptions['checkListtwo']}`,
-        ChildrenModule: scene,
-    });
-    callUIInteraction({
-        ModuleName: '假设分析',
-        FunctionName: `${descriptions['checkListtwo']}`,
-        ChildrenModule: scene,
-        State: checkListtwo.value[0]
-    });
-    callUIInteraction({
-        ModuleName: '假设分析',
-        FunctionName: `${descriptions['checkListtwo']}`,
-        ChildrenModule: scene,
-        Dimension: `二维`,
-        State: true
+        FunctionName: `减灾措施情景库无减灾设施`,
+        State: false
     });
     console.log('假设分析', `水位情景库`, inputvalue.value);
-    console.log('假设分析', `${descriptions['checkListtwo']}`, scene);
-    console.log('假设分析', `${descriptions['checkListtwo']}`, scene, checkListtwo.value[0]);
-    console.log('假设分析', `${descriptions['checkListtwo']}`, scene, `二维`, true);
+    console.log('假设分析', `减灾措施情景库无减灾设施`, false);
 });
 
 watch(checkListone, (newValue, oldValue) => {
@@ -189,6 +168,12 @@ watch(checkListone, (newValue, oldValue) => {
         setTimeout(() => {
             callUIInteraction({
                 ModuleName: '假设分析',
+                FunctionName: `水位情景库`,
+                State: inputvalue.value
+            });
+            console.log('假设分析', `水位情景库`, inputvalue.value);
+            callUIInteraction({
+                ModuleName: '假设分析',
                 FunctionName: `${descriptions['checkListone']}${added[added.length - 1]}`,
                 State: true
             });
@@ -197,36 +182,66 @@ watch(checkListone, (newValue, oldValue) => {
     }
 });
 
+let isdimension = '';
+if (dimensionvalue.value === true) {
+    isdimension = '二维';
+} else if (dimensionvalue.value === false) {
+    isdimension = '三维';
+}
+watch(dimensionvalue, (newValue) => {
+    isdimension = newValue ? '二维' : '三维';
+});
+
+
 watch(checkListtwo, (newValue, oldValue) => {
-    const added = newValue.filter(item => !oldValue.includes(item));
-    if (added.length) {
-        const numericValue = parseInt(added[added.length - 1]);
-        const stringValue = numericValue.toString();
+    if (newValue == '') {
         callUIInteraction({
             ModuleName: '假设分析',
             FunctionName: `${descriptions['checkListtwo']}`,
-            ChildrenModule: scene,
-            State: stringValue
+            State: false
         });
-        console.log('假设分析', `${descriptions['checkListtwo']}`, stringValue);
+        console.log('假设分析', `${descriptions['checkListtwo']}`, false);
     }
 });
 
 const handleCheckChange = (listName) => {
     const list = listName === 'checkListone' ? checkListone : checkListtwo;
-
     // 如果是 checkListtwo，确保只能选择一个
     if (listName === 'checkListtwo') {
         if (list.value.length > 1) {
             list.value = [list.value[list.value.length - 1]]; // 保留最后一个选项
         }
-
         // 如果当前选项被取消，恢复到上一个选项
         if (list.value.length === 0) {
             list.value = [lastSelected.value]; // 恢复到上一个选项
         } else {
             lastSelected.value = list.value[0]; // 更新上一个选项
         }
+        let scene = '';
+        if (checkList.value[0] === '海浪情景') {
+            scene = '海浪情景';
+        } else if (checkList.value[0] === '淹没情景') {
+            scene = '淹没情景';
+        }
+        if (scene === '海浪情景') {
+            callUIInteraction({
+                ModuleName: '假设分析',
+                FunctionName: `${descriptions['checkListtwo']}`,
+                ChildrenModule: scene,
+                State: lastSelected.value,
+                Dimension: isdimension
+            });
+            console.log('假设分析', `${descriptions['checkListtwo']}`, scene, lastSelected.value, isdimension);
+        } else if (scene === '淹没情景') {
+            callUIInteraction({
+                ModuleName: '假设分析',
+                FunctionName: `${descriptions['checkListtwo']}`,
+                ChildrenModule: scene,
+                State: lastSelected.value
+            });
+            console.log('假设分析', `${descriptions['checkListtwo']}`, scene, lastSelected.value);
+        }
+
     } else {
         // 如果选中多个，保留最后一个
         if (list.value.length > 1) {
@@ -235,7 +250,6 @@ const handleCheckChange = (listName) => {
     }
 };
 </script>
-
 
 <style scoped>
 .left-menu {
@@ -463,7 +477,7 @@ const handleCheckChange = (listName) => {
     top: 60px;
 }
 
-.radiobox {
+.checkbox {
     width: 100%;
     height: 60px;
     display: flex;
